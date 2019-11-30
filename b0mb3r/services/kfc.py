a5838a0ce7a932bcf3fc5b9327b2896ee7c9913f
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:34:03 +0300

    Delete kfc.py

--- b0mb3r/services/kfc.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class FindClone(Service):
-    async def run(self):
-        await self.client.post('https://app-api.kfc.ru/api/v1/common/auth/send-validation-sms', json={'phone': '+' + self.formatted_phone})
