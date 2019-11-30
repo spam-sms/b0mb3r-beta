3f0026de17c5980e7471191fb1f546d7b4311a2e
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 16:19:35 +0300

    Create kapibara.py

---
+++ b0mb3r/services/kapibara.py
@@ -0,0 +1,8 @@
+from service import Service
+
+
+class Kapibaras(Service):
+    async def run(self):
+        if self.country_code == 'ru':
+            phone = f'+{self.formatted_phone[0]}({self.formatted_phone[1:4]})-{self.formatted_phone[4:7]}-{self.formatted_phone[7:11]}'
+            await self.client.post('https://kapibaras.ru/api/lk/sendCode', json={'phone': phone, 'city': 1})
