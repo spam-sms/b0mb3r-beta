0891b9dad452641bc0d78d868a98c899f261d5ec
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:43:50 +0300

    Delete wowworks.py

--- b0mb3r/services/wowworks.py
+++
@@ -1,7 +0,0 @@
-from service import Service
-
-
-class AtPrime(Service):
-    async def run(self):
-        await self.client.post("https://api.wowworks.ru/v2/site/send-code",
-                               json={"phone": self.formatted_phone, "type": 2})
