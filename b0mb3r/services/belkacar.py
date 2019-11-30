443ce8abc2864d7e19a77b935b7725f58623e3ad
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:37:42 +0300

    Delete belkacar.py

--- b0mb3r/services/belkacar.py
+++
@@ -1,7 +0,0 @@
-from service import Service
-
-
-class BelkaCar(Service):
-    async def run(self):
-        await self.client.post('https://belkacar.ru/get-confirmation-code',
-                          data={'phone': self.formatted_phone})
