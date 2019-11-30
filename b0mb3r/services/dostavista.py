5a92a33558cc9818be4aa7788620242d9f5581b8
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 16:45:36 +0300

    Delete dostavista.py

--- b0mb3r/services/dostavista.py
+++
@@ -1,8 +0,0 @@
-from service import Service
-
-
-class Dostavista(Service):
-    async def run(self):
-        phone = f'{self.formatted_phone[1:4]} {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-        cookies = self.client.get('https://dostavista.ru/').cookies
-        await self.client.post('https://dostavista.ru/backend/send-verification-sms', cookies=cookies, data={'phone': phone})
