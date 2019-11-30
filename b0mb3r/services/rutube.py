e7fe1bb9d494c5f3439adb4740456fe2e6b0d337
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:42:11 +0300

    Delete rutube.py

--- b0mb3r/services/rutube.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class RuTube(Service):
-    async def run(self):
-        await self.client.post('https://rutube.ru/api/accounts/sendpass/phone', data={'phone': '+' + self.formatted_phone})
