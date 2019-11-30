208d27b7fa4e5876c8af2e2c5fa945900c85dbf0
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:39:14 +0300

    Delete plink.py

--- b0mb3r/services/plink.py
+++
@@ -1,7 +0,0 @@
-from service import Service
-
-
-class Plink(Service):
-    async def run(self):
-        await self.client.post('https://plink.tech/register/',
-                          json={"phone": self.formatted_phone})
