cc5b5f7924fed7a6cd9e902c452776a642e5656c
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 16:30:39 +0300

    Update aptekaru.py

--- b0mb3r/services/aptekaru.py
+++ b0mb3r/services/aptekaru.py
@@ -5,4 +5,4 @@ from service import Service
 class AptekaRu(Service):
     async def run(self):
         phone = f'+7 ({self.formatted_phone[1:4]}) {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-        await self.session.post('https://apteka.ru/_action/auth/getForm/', data={'form[LOGIN]': phone, 'get-new-password': 'Получите пароль по SMS', 'formType': 'simple', 'utc_offset': '180'})
+        await self.client.post('https://apteka.ru/_action/auth/getForm/', data={'form[LOGIN]': phone, 'get-new-password': 'Получите пароль по SMS', 'formType': 'simple', 'utc_offset': '180'})
