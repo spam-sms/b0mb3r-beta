936f150736d6718d7f3df5ca7bc82c0715402c17
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 16:46:12 +0300

    Delete psdelivery.py

--- b0mb3r/services/psdelivery.py
+++
@@ -1,8 +0,0 @@
-from service import Service
-
-
-class Psdelivery(Service):
-    async def run(self):
-        phone = f'+{self.formatted_phone[0]} ({self.formatted_phone[1:4]}) {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-        cookies = self.client.get('https://ps.delivery/').cookies
-        self.client.post('https://ps.delivery/wp-admin/admin-ajax.php', cookies=cookies, data={'tel': phone, 'redirect_to': '', 'rememberme': 'forever', 'undefined': '', 'action': 'login'})
