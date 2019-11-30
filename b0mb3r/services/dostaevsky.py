37812537f11e3177c9e83ee29bf736033b5cda5e
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:38:06 +0300

    Delete dostaevsky.py

--- b0mb3r/services/dostaevsky.py
+++
@@ -1,7 +0,0 @@
-from service import Service
-
-
-class Dostaevsky(Service):
-    async def run(self):
-        phone = f'{self.formatted_phone[0]} {self.formatted_phone[1:4]} {self.formatted_phone[4:7]}-{self.formatted_phone[7:9]}-{self.formatted_phone[9:11]}'
-        await self.client.post('https://dostaevsky.ru/auth/send-sms', data={'phone': phone})
