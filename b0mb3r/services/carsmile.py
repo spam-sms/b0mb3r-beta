10f0fc8d717820e3e35e45364d90f0751165be55
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:39:39 +0300

    Delete carsmile.py

--- b0mb3r/services/carsmile.py
+++
@@ -1,8 +0,0 @@
-from service import Service
-
-
-class CarSmile(Service):
-    async def run(self):
-        await self.client.post("https://api.carsmile.com/",
-                               json={"operationName": "enterPhone", "variables": {"phone": self.formatted_phone},
-                                     "query": "mutation enterPhone($phone: String!) {\n  enterPhone(phone: $phone)\n}\n"})
