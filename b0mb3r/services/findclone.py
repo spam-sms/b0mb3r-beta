742e4e3e71bbb8b42d07519fe01de2ba877a6cea
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:44:50 +0300

    Delete findclone.py

--- b0mb3r/services/findclone.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class FindClone(Service):
-    async def run(self):
-        await self.client.get('https://findclone.ru/register', params={'phone': '+' + self.formatted_phone})
