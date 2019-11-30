018b89eaf8bcc18b45a96b8ec894b194f9fc71b0
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:36:33 +0300

    Delete atprime.py

--- b0mb3r/services/atprime.py
+++
@@ -1,7 +0,0 @@
-from service import Service
-
-
-class AtPrime(Service):
-    async def run(self):
-        await self.client.post("https://api-prime.anytime.global/api/v2/auth/sendVerificationCode",
-                               data={"phone": self.formatted_phone})
