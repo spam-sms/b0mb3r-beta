570ed026b9e3a2f6843d1d7bdb846c7325d8096a
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:38:20 +0300

    Delete citilink.py

--- b0mb3r/services/citilink.py
+++
@@ -1,6 +0,0 @@
-from service import Service
-
-
-class Citilink(Service):
-    async def run(self):
-        await self.client.post('https://www.citilink.ru/registration/confirm/phone/+' + self.formatted_phone + '/')
