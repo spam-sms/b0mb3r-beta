3a4487fe885a6155d9f35db98a631e4f5818045f
spam-sms <54709225+spam-sms@users.noreply.github.com>
2019-11-30 18:41:29 +0300

    Delete smsgorod.py

--- b0mb3r/services/smsgorod.py
+++
@@ -1,7 +0,0 @@
-from service import Service
-
-
-class SMSGorod(Service):
-    async def run(self):
-        await self.client.post("http://smsgorod.ru/sendsms.php",
-                               data={"number": self.formatted_phone})
